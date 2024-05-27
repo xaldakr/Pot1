f35cc5e682c171911c1312877b4703e6c1574dd3
xaldakr <xavieravilaposada@gmail.com>
2024-05-26 23:29:09 -0600

    Some thingas

--- Pot1 API/Controllers/TicketController.cs
+++ Pot1 API/Controllers/TicketController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Pot1_API.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Pot1_API.Controllers
 {
@@ -15,14 +17,13 @@ namespace Pot1_API.Controllers
         }
 
         [HttpGet]
-        [Route("ObtenerTodo")]
-        public IActionResult GetAll()
+        [Route("ObtenerDash")]
+        public IActionResult GetDash()
         {
             var listicket = (from e in _Contexto.Tickets
-                             join c in _Contexto.Usuarios
-                             on e.id_cliente equals c.id_usuario
-                             join em in _Contexto.Usuarios
-                             on e.id_encargado equals em.id_usuario
+                             join c in _Contexto.Usuarios on e.id_cliente equals c.id_usuario
+                             join em in _Contexto.Usuarios on e.id_encargado equals em.id_usuario into emJoin
+                             from em in emJoin.DefaultIfEmpty()
                              select new
                              {
                                  Id = e.id_ticket,
@@ -31,10 +32,41 @@ namespace Pot1_API.Controllers
                                  Fecha = e.fecha.ToString("dd/MMMM/yyyy"),
                                  Estado = e.estado,
                                  Cliente = c.nombre + " " + c.apellido,
-                                 Empleado = em.nombre + " " + em.apellido,
+                                 Empleado = em == null ? null : em.nombre + " " + em.apellido,
                                  Correo = c.email,
-                             } ).OrderByDescending(res => res.FechaDate).ToList();
-            if (listicket.Count ==0 )
+                             })
+                    .OrderByDescending(res => res.FechaDate)
+                    .Take(30)
+                    .ToList();
+            if (listicket.Count == 0)
+            {
+                return NotFound("No se han encontrado tickets");
+            }
+            return Ok(listicket);
+        }
+
+        [HttpGet]
+        [Route("ObtenerTodo")]
+        public IActionResult GetAll()
+        {
+            var listicket = (from e in _Contexto.Tickets
+                             join c in _Contexto.Usuarios on e.id_cliente equals c.id_usuario
+                             join em in _Contexto.Usuarios on e.id_encargado equals em.id_usuario into emJoin
+                             from em in emJoin.DefaultIfEmpty()
+                             select new
+                             {
+                                 Id = e.id_ticket,
+                                 Servicio = e.servicio,
+                                 FechaDate = e.fecha,
+                                 Fecha = e.fecha.ToString("dd/MMMM/yyyy"),
+                                 Estado = e.estado,
+                                 Cliente = c.nombre + " " + c.apellido,
+                                 Empleado = em == null ? null : em.nombre + " " + em.apellido,
+                                 Correo = c.email,
+                             })
+                    .OrderByDescending(res => res.FechaDate)
+                    .ToList();
+            if (listicket.Count == 0 )
             {
                 return NotFound("No se han encontrado tickets");
             }
@@ -48,8 +80,8 @@ namespace Pot1_API.Controllers
             var listicket = (from e in _Contexto.Tickets
                              join c in _Contexto.Usuarios
                              on e.id_cliente equals c.id_usuario
-                             join em in _Contexto.Usuarios
-                             on e.id_encargado equals em.id_usuario
+                             join em in _Contexto.Usuarios on e.id_encargado equals em.id_usuario into emJoin
+                             from em in emJoin.DefaultIfEmpty()
                              where c.id_usuario == id
                              select new
                              {
@@ -59,7 +91,7 @@ namespace Pot1_API.Controllers
                                  Fecha = e.fecha.ToString("dd/MMMM/yyyy"),
                                  Estado = e.estado,
                                  Cliente = c.nombre + " " + c.apellido,
-                                 Empleado = em.nombre + " " + em.apellido,
+                                 Empleado = em == null ? null : em.nombre + " " + em.apellido,
                                  Correo = c.email,
                              }).OrderByDescending(res => res.FechaDate).ToList();
             if (listicket.Count == 0)
@@ -78,7 +110,7 @@ namespace Pot1_API.Controllers
                              on e.id_cliente equals c.id_usuario
                              join em in _Contexto.Usuarios
                              on e.id_encargado equals em.id_usuario
-                             where em.id_usuario == id
+                             where em.id_usuario == id && !e.estado.Contains("RESUELTO")
                              select new
                              {
                                  Id = e.id_ticket,
@@ -90,6 +122,82 @@ namespace Pot1_API.Controllers
                                  Empleado = em.nombre + " " + em.apellido,
                                  Correo = c.email,
                              }).OrderByDescending(res => res.FechaDate).ToList();
+            if (listicket.Count == 0)
+            {
+                return NotFound("No se han encontrado tickets");
+            }
+            return Ok(listicket);
+        }
+        [HttpGet]
+        [Route("ObtenerPorEstado")]
+        public IActionResult GetPorEstado([FromBody] JObject data)
+        {
+            if (data == null)
+            {
+                return BadRequest("No se ha enviado nada en el JSON.");
+            }
+
+            // Descomponer y validar los datos
+            DateTime inicio;
+            DateTime fin;
+            int tipo;
+            //  1 es Resueltos
+            //  2 es En Progreso
+            //  3 es Abiertos
+
+            try
+            {
+                inicio = data.Value<DateTime>("inicio");
+                fin = data.Value<DateTime>("fin");
+                tipo = data.Value<int>("tipo");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Formato inválido.");
+            }
+            if(tipo > 3 || tipo < 1){
+                return BadRequest("Tipo no especificado");
+            }
+            // Validar las fechas
+            if (inicio > fin || inicio == default(DateTime) || fin == default(DateTime))
+            {
+                inicio = DateTime.Today;
+                fin = DateTime.Today.AddDays(1).AddTicks(-1); // Fin del día de hoy
+            }
+            var listicket1 = (from e in _Contexto.Tickets
+                             join c in _Contexto.Usuarios
+                             on e.id_cliente equals c.id_usuario
+                             join em in _Contexto.Usuarios on e.id_encargado equals em.id_usuario into emJoin
+                             from em in emJoin.DefaultIfEmpty()
+                             where e.fecha >= inicio && e.fecha <= fin
+                             select new
+                             {
+                                 Id = e.id_ticket,
+                                 Servicio = e.servicio,
+                                 FechaDate = e.fecha,
+                                 Fecha = e.fecha.ToString("dd/MMMM/yyyy"),
+                                 Estado = e.estado,
+                                 Cliente = c.nombre + " " + c.apellido,
+                                 Empleado = em == null ? null : em.nombre + " " + em.apellido,
+                                 Correo = c.email,
+                             }).OrderByDescending(res => res.FechaDate).ToList();
+            var listicket = listicket1;
+            if(tipo == 1)
+            {
+                listicket = (from e in listicket1
+                             where e.Estado.Contains("RESUELTO")
+                             select e).OrderByDescending(res => res.FechaDate).ToList();
+            } else if (tipo == 2)
+            {
+                listicket = (from e in listicket1
+                             where !e.Estado.Contains("RESUELTO") && !e.Estado.Contains("CREADO")
+                             select e).OrderByDescending(res => res.FechaDate).ToList();
+            } else
+            {
+                listicket = (from e in listicket1
+                             where e.Estado.Contains("CREADO")
+                             select e).OrderByDescending(res => res.FechaDate).ToList();
+            }
             if (listicket.Count == 0)
             {
                 return NotFound("No se han encontrado tickets");
---
+++ Pot1 API/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pot1_API.Models;
+
+namespace Pot1_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuariosController : Controller
+    {
+        private readonly Pot1Context _Contexto;
+
+        public UsuariosController(Pot1Context Contexto)
+        {
+            _Contexto = Contexto;
+        }
+        [HttpGet]
+        [Route("ObtenerDash")]
+        public IActionResult GetDash()
+        {
+
+        }
+    }
+}
